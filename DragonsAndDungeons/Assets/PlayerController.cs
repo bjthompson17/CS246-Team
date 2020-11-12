31d76f282ad81d78664d3eb5765cb02389072085
bjthompson17 <tho17054@byui.edu>
2020-11-12 15:58:05 -0700

    player movment and collisions implemented

---
+++ DragonsAndDungeons/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 dir = new Vector2(0,0);
+    // Update is called once per frame
+    void Update()
+    {
+        dir.x = Input.GetAxisRaw("Horizontal");
+        dir.y = Input.GetAxisRaw("Vertical");
+        dir.Normalize();
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + dir * speed * Time.deltaTime);
+    }
+}
